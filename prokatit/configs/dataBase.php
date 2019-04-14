b95e0612b9e4751a6f1f7f4be68ef38481bb9fc9
Максим Плющ <mplyushch@ivi.ru>
2019-04-14 03:17:33 +0300

    Define constants

--- prokatit/configs/dataBase.php
+++ prokatit/configs/dataBase.php
@@ -1,6 +1,6 @@
 <?
-define("DB_TYPE", 'mysql');
-define("DB_HOST", 'localhost');
-define("DB_NAME", 'prokatit');
-define("DB_USER", 'root');
-define("DB_PASSWORD", '50198581994meks');
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'prokatit');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '50198581994meks');
