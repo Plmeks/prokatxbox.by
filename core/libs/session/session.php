00c4a399509ad0fd1951283046648220a7d1065c
Максим Плющ <mplyushch@ivi.ru>
2019-04-14 15:03:27 +0300

    Check session start

--- core/libs/session/session.php
+++ core/libs/session/session.php
@@ -1,7 +1,9 @@
 <?php
 	class Session {
 		public static function init() {
-			session_start();
+			if (session_status() == PHP_SESSION_NONE) {
+				session_start();
+			}
 		}
 
 		public static function set($key, $value) {
